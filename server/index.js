877233a538ece5274f335d20ac97dafc6a307b75
Jason <jasonmarble@gmail.com>
2021-07-24 11:37:08 -0600

    building app.all, option finished

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+// const axios = require('axios');
+const API_KEY = require('../config');
 
 const port = 3000;
 
@@ -8,9 +10,20 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 // routes
+
+app.all('*', (req, res, next) => {
+  const options = {
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld${req.url}`,
+    data: req.body,
+    method: req.method,
+    headers: {
+      Authorization: API_KEY,
+    },
+  };
+});
 
 app.listen(port, () => {
   console.log('hello from the server on port 3000');
