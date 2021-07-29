5b570c24aad56c339209fc61e898927f3ad68341
Jason <jasonmarble@gmail.com>
2021-07-29 14:26:03 -0600

    added helpful component

--- client/src/components/Q&A/QuestionRow.jsx
+++ client/src/components/Q&A/QuestionRow.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable import/extensions */
 import React from 'react';
 import AnswerRow from './AnswerRow.jsx';
+import Helpful from './Helpful.jsx';
 
 export default function QuestionRow() {
   return (
     <div style={{ border: '2px solid slateblue' }}>
       Hello from Question Row
+      <Helpful />
       <AnswerRow />
     </div>
   );
