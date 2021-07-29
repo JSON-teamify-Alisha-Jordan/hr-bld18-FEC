922928a4135cd7e873cc3927481b9d9b418ad9f4
Jason <jasonmarble@gmail.com>
2021-07-29 14:09:18 -0600

    questions & answers framework established

---
+++ client/src/components/Q&A/QA.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchBar from './SearchBar.jsx';
+import QuestionRow from './QuestionRow.jsx';
+import AddQuestion from './AddQuestion.jsx';
+
+export default function QA() {
+  return (
+    <div style={{ border: '2px solid slateblue' }}>
+      Greetings from QA
+      <h2>Questions & Answers</h2>
+      <SearchBar />
+      <QuestionRow />
+      <AddQuestion />
+    </div>
+  );
+}
---
+++ client/src/components/Q&A/QuestionRow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AnswerRow from './AnswerRow.jsx';
+
+export default function QuestionRow() {
+  return (
+    <div style={{ border: '2px solid slateblue' }}>
+      Hello from Question Row
+      <AnswerRow />
+    </div>
+  );
+}
