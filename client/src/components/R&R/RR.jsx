22ffe5ed1fd4819cf8a6dfd02dd7cdf74aaaa8a3
Jordan Abbasi <jordanvabbasi@gmail.com>
2021-07-31 20:17:23 -0600

    Adds id to heading so that we can link to Ratings and Reviews section

--- client/src/components/R&R/RR.jsx
+++ client/src/components/R&R/RR.jsx
@@ -6,7 +6,7 @@ import ReviewsList from './ReviewsList.jsx';
 export default function RR() {
   return (
     <div className="rr-container">
-      <h2 className="rr-header">Ratings & Reviews</h2>
+      <h2 id="ratings_and_reviews" className="rr-header">Ratings & Reviews</h2>
       <Breakdown />
       <ReviewsList />
     </div>
