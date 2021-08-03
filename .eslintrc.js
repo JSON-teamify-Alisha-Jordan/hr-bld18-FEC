7032a787de6c3ce50e1b8935cc349e270ce48a16
Alisha Edington <alisha_56789@yahoo.com>
2021-08-03 14:02:56 -0600

    changed something to show Truman and forgot to put it back

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,7 +27,8 @@ module.exports = {
     'react/jsx-one-expression-per-line': 0,
     'react/prop-types': 0,
     'import/extensions': 0,
-    'jsx-a11y/click-events-have-key-events': 2,
+    'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
+    'no-restricted-syntax': 0,
   },
 };
