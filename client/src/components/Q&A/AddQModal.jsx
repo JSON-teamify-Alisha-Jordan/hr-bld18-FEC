1a72ee9bc9a4b13ca0d83fc4f93cfc571122e711
Jason <jasonmarble@gmail.com>
2021-08-03 13:20:59 -0600

    refactored into form

--- client/src/components/Q&A/AddQModal.jsx
+++ client/src/components/Q&A/AddQModal.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export default function AddQModal({ show, setShow }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+  }
+
   return (
     <div
       className="modal-wrapper"
@@ -14,22 +19,32 @@ export default function AddQModal({ show, setShow }) {
       }}
     >
       <div className="modal-header">
-        <b><h2>Add a Question</h2></b>
+        <b><h2>Ask Your Question</h2></b>
+        <em><h3>[Product Name Here]</h3></em>
         <span
           onClick={() => setShow(false)}
-          className="qa-close-modal-btn"
+          className="close-modal-btn"
         ><b>X</b>
         </span>
       </div>
       <div className="modal-content">
-        <div className="modal-body">
+        <form
+          className="modal-body"
+          onSubmit={handleSubmit}
+        >
           <div className="modal-top-info">
             <h3>Nickname</h3>
             <h3>Email</h3>
           </div>
           <div className="modal-nickname-email">
-            <input type="text" placeholder="Example: jackson11!" />
-            <input type="text" placeholder="aperson@gmail.com" />
+            <input
+              type="text"
+              placeholder="Example: jackson11!"
+            />
+            <input
+              type="text"
+              placeholder="aperson@gmail.com"
+            />
           </div>
           <div className="modal-info">
             <span>For privacy reasons, do not use your full name or email address</span>
@@ -37,24 +52,24 @@ export default function AddQModal({ show, setShow }) {
           </div>
           <textarea
             className="modal-input"
-            placeholder="Why did you like the product or not?"
+            placeholder="What would you like to ask about this product?"
           />
-        </div>
-        <div className="modal-footer">
-          <button
-            className="btn-add"
-            type="button"
-          >
-            <b>Add Question</b>
-          </button>
-          <button
-            className="btn-close"
-            onClick={() => setShow(false)}
-            type="button"
-          >
-            <b>Close</b>
-          </button>
-        </div>
+          <div className="modal-footer">
+            <button
+              className="btn-add"
+              type="submit"
+            >
+              <b>Add Question</b>
+            </button>
+            <button
+              className="btn-close"
+              onClick={() => setShow(false)}
+              type="button"
+            >
+              <b>Close</b>
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
