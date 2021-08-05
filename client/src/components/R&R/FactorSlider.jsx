117fbe887088083e2b744731bf1ba2e0f2977b9e
Jordan Abbasi <jordanvabbasi@gmail.com>
2021-08-04 22:17:42 -0600

    Minimized inline CSS in FactorSlider.jsx

--- client/src/components/R&R/FactorSlider.jsx
+++ client/src/components/R&R/FactorSlider.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
 export default function FactorSlider({ name, value }) {
+  // convert value (1-5 scale) to a percentage - 1 is 0%, 5 is 100%
   const percent = ((value - 1) / 4) * 100;
 
   // positioning for triangle in slider
   const minPosition = -0.5;
   const maxPosition = 12.45;
+  // calculate position where 0% is minPosition, 100% is maxPosition
+  const offset = minPosition + (percent / 100) * (maxPosition - minPosition);
 
   return (
     <div className="rr-factor-slider-container">
       <h4>{name}</h4>
       <div className="rr-factor-slider">
-        <span style={{ left: `${minPosition + (percent / 100) * (maxPosition - minPosition)}rem` }}>&#9660;</span>
+        <span style={{ left: `${offset}rem` }}>&#9660;</span>
       </div>
-      <div>
+      <div className="rr-factor-slider-labels">
         <span className="rr-factor-slider-label">Too small</span>
-        <span className="rr-factor-slider-label" style={{ float: 'right' }}>Too large</span>
+        <span className="rr-factor-slider-label rr-right-label">Too large</span>
       </div>
     </div>
   );
--- client/src/components/R&R/ReviewsList.jsx
+++ client/src/components/R&R/ReviewsList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ProductContext from '../../context';
 import ReviewsListHeader from './ReviewsListHeader';
 import Review from './Review';
@@ -7,13 +7,19 @@ import ReviewsListButtonRow from './ReviewsListButtonRow';
 export default function ReviewsList() {
   const { reviews } = useContext(ProductContext);
 
+  const [count, setCount] = useState(2);
+
+  function showTwoMoreReviews() {
+    setCount((prevCount) => prevCount + 2);
+  }
+
   return (
     <div className="reviews-list">
       <ReviewsListHeader numReviews={reviews.length} />
       <div className="reviews-container">
-        {reviews.map((review) => <Review review={review} key={review.review_id} />)}
+        {reviews.slice(0, count).map((review) => <Review review={review} key={review.review_id} />)}
       </div>
-      <ReviewsListButtonRow />
+      <ReviewsListButtonRow showTwoMoreReviews={showTwoMoreReviews} />
     </div>
   );
 }
