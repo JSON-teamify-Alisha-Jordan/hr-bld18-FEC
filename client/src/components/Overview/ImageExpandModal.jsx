1002fbf206601ad86fbe2efa9e86ff28cd2c6100
Alisha Edington <alisha_56789@yahoo.com>
2021-08-07 11:22:58 -0600

    zooming feature mostly working.

--- client/src/components/Overview/ImageExpandModal.jsx
+++ client/src/components/Overview/ImageExpandModal.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/extensions */
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef, useLayoutEffect } from 'react';
 import ProductContext from '../../context';
 
 export default function ImageExpandModal({
@@ -10,6 +10,32 @@ export default function ImageExpandModal({
   maxIndex,
 }) {
   const { showImageModal, setShowImageModal } = useContext(ProductContext);
+  const imageRef = useRef();
+  const [mousePosition, setMousePosition] = useState({ left: 0, top: 0 });
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  function handleMouseMove(event) {
+    console.log(event.target);
+    console.log(imageRef)
+    setMousePosition({ left: event.pageX, top: event.pageY });
+    const bgPosX = ((mousePosition.left / dimensions.width) * 100);
+    const bgPosY = ((mousePosition.top / dimensions.height) * 100);
+    imageRef.current.style.backgroundPosition = `${bgPosX}% ${bgPosY}%`;
+  }
+
+  function handleMouseLeave(event) {
+    imageRef.current.style.backgroundPosition = 'center';
+  }
+
+  useLayoutEffect(() => {
+    if (imageRef.current) {
+      setDimensions({
+        width: imageRef.current.offsetWidth,
+        height: imageRef.current.offsetHeight,
+      });
+    }
+  }, []);
+
   return (
     <div
       className="modal-wrapper"
@@ -22,16 +48,12 @@ export default function ImageExpandModal({
     >
       <div className="modal-content">
         <div className="modal-footer">
-          <button
-            className="btn-close"
-            onClick={() => setShowImageModal(false)}
-            type="button"
-          >
+          <button className="btn-close" onClick={() => setShowImageModal(false)} type="button">
             Close
           </button>
         </div>
         <div className="modal-image-container">
-          <img className="overview-modal-image" src={currentPhoto} alt="cool people in cool clothing" />
+          <div ref={imageRef} onMouseMove={(event) => handleMouseMove(event)} onMouseLeave={(event) => handleMouseLeave(event)} className="overview-modal-image" style={{ backgroundImage: `url(${currentPhoto})` }} />
           <div className="arrow-container-expanded">
             {currentPhotoIndex === 0 ? <div />
               : <div><i onClick={previousPhoto} className="fas image-arrow-expanded fa-chevron-left" /></div>}
